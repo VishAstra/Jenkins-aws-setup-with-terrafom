17479a413ddf8d362f24b34302d487c13ca0568c
Vishnu Chandran <vshnu911@gmail.com>
2023-10-03 23:30:20 +0530

    Jenkins configuration using Terraform

--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0" #You can change the version as you required.
+      version = "~> 3.0" #You can change the version as per your requirement.
     }
   }
 }
